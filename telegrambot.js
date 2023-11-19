417d916bc0826ef94fc523bc046663c533990b8d
Jean Staffe <69088819+IceroDev@users.noreply.github.com>
2023-11-19 16:04:34 +0100

    optimisations

--- telegrambot.js
+++ telegrambot.js
@@ -4,15 +4,18 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_TOKEN;
 const bot = new TelegramBot(token, {polling: true});
 
+const TELEGRAM_URL = process.env.TELEGRAM_URL;
+const WEBPORT = process.env.WEBPORT;
+
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
     
     if (text == '/start') {
-        bot.sendMessage(chatId, "Мы не принимаем просто так, поэтому, пожалуйста, пройдите через наш сайт проверки, чтобы убедиться, что вы действительно один из нас: \nhttp://X.X.X.X:"+process.env.WEBPORT+"/");
+        bot.sendMessage(chatId, "Мы не принимаем просто так, поэтому, пожалуйста, пройдите через наш сайт проверки, чтобы убедиться, что вы действительно один из нас: \nhttp://X.X.X.X:"+WEBPORT+"/");
     }
 
     if (text == '/join dPsWu72M23nyL2'){
-        bot.sendMessage(chatId, process.env.TELEGRAM_URL);
+        bot.sendMessage(chatId, TELEGRAM_URL);
     }
 })
